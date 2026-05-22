e7c9fe42fd62a8e89534412b15332976c2f78d16
moicenti <00054123@uca.edu.sv>
2026-05-21 18:21:48 -0600

    added yaml with api petitions

--- demo/src/main/java/com/example/demo/controllers/SpecimenController.java
+++ demo/src/main/java/com/example/demo/controllers/SpecimenController.java
@@ -23,7 +23,7 @@ public class SpecimenController {
     @GetMapping("/get")
     public ResponseEntity<GeneralResponse> getAll
             (@RequestParam(defaultValue = "0") int page,
-             @RequestParam(defaultValue = "10") int size,
+             @RequestParam(defaultValue = "5") int size,
              @RequestParam(defaultValue = "id") String sortBy,
              @RequestParam(defaultValue = "asc") String sortOrder
             ){
